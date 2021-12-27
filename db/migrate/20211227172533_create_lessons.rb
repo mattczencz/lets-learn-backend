c4817ac753c204bfbaf32eb90281e0389980f514
Matt Czencz <mattczencz24@gmail.com>
2021-12-27 10:37:41 -0700

    Adding backend API

---
+++ db/migrate/20211227172533_create_lessons.rb
@@ -0,0 +1,11 @@
+class CreateLessons < ActiveRecord::Migration[6.1]
+  def change
+    create_table :lessons do |t|
+      t.string :title
+      t.string :description
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
